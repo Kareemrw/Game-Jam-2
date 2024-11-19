b19064a587e34463d6221db770f961bbf01a5c64
slu51 <slu51@ucsc.edu>
2024-11-18 20:51:35 -0800

    1
    1

--- Gravity Flux/Assets/Scripts/Key.cs
+++ Gravity Flux/Assets/Scripts/Key.cs
@@ -2,16 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Key : MonoBehaviour
-{   public GameObject door;
-    private void OnTriggerEnter2D(Collider2D col)
+public class KeyCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
     {
-        if (col.CompareTag("Player"))
-        {
-            Debug.Log("Key Picked Up");
-            door.GetComponent<BoxCollider2D>().enabled = false;
-            this.gameObject.SetActive(false)
-        }
+        
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player has acquired key!");
+            gameObject.SetActive(false);
+        }
+    }
 }
